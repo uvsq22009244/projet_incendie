27fd23446da9652b79f148d0064bad913e9805b6
Lilian Joannet <lilian.joannet@ens.uvsq.fr>
2021-02-12 11:11:03 +0100

    rhjieotyjhtriotyuytyu

--- incendie.py
+++ incendie.py
@@ -1,18 +1,14 @@
 ################################################
 #Groupe BI TD1
 #Princesse MOMO
-#Alenxadra SCHMIDT
+#Alexandra SCHMIDT
 #Malak LALAMI
 #Nolwenn CORIC
 #Lilian JOANNET
 #Tristan TOLENTINO
 #https://github.com/uvsq22009244/projet_incendie
 ################################################
-dans 1 heure
 
-
-
-AHHHHHHHHHH
 petit test
-TOLENTINOOOOOOOOOOOOOOOOOOOOOOOOOO
+
 AHHHHHHHHHH
