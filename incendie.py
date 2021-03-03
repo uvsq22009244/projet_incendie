a992726b30e73ed4bc0bf47918c9a0bae75ae3e1
uvsq22009244 <princesse.momo@ens.uvsq.fr>
2021-03-03 18:50:44 +0100

    ajout de code

--- incendie.py
+++ incendie.py
@@ -1,26 +1,47 @@
 ################################################
-#Groupe 1 BI TD1
-#Princesse MOMO
-#Alexandra SCHMIDT
-#Malak LALAMI
-#Nolwenn CORIC
-#Lilian JOANNET
-#Tristan TOLENTINO
-#https://github.com/uvsq22009244/projet_incendie
+
+# Groupe 1 BI TD1
+# Princesse MOMO
+# Alexandra SCHMIDT
+# Malak LALAMI
+# Nolwenn CORIC
+# Lilian JOANNET
+# Tristan TOLENTINO
+# https://github.com/uvsq22009244/projet_incendie
+
 ################################################
-c'est dur
+# import des modules
 
-petit test
-hazar
-en même temps
-rajout
-je marque un truc
-je m'ennuies
-OK
-rajout
-yo je galère
-yolo
-enfin
+import tkinter as tk
 
-Salut lilian
-jai faimmmmmmmmmmmmm
+
+################################################
+# constantes
+HAUTEUR = 400
+LARGEUR = 400
+COULEUR_FOND = "red"
+
+
+################################################
+# variables globales
+
+
+################################################
+# fonctions
+
+
+################################################
+# programme principale
+
+racine = tk.Tk()
+racine.title("Incendie")
+
+# création des widgets
+canvas = tk.Canvas(racine, width = HAUTEUR, height = LARGEUR, bg = COULEUR_FOND)
+
+# placement des widgets
+canvas.grid(row=0)
+
+# liaison des événements
+
+racine.mainloop()
