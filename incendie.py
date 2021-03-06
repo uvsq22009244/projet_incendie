c35c10e0fa265c52182e6588eaa3cb41e8f7bdfd
uvsq22009244 <princesse.momo@ens.uvsq.fr>
2021-03-06 22:45:18 +0100

    ajout de couleur du terrain de façon aléatoire

--- incendie.py
+++ incendie.py
@@ -13,7 +13,7 @@
 # import des modules
 
 import tkinter as tk
-import random as rd
+import random 
 
 ################################################
 # constantes
@@ -21,9 +21,13 @@ HAUTEUR = 400
 LARGEUR = 400
 
 
-COULEUR_FOND = "red"
+COULEUR_FOND = "grey50"
 COTE = 100
-COULEUR_QUADR = "grey20"
+COULEUR_QUADR = "grey60"
+COULEUR_EAU = "blue"
+COULEUR_FORET = "green"
+COULEUR_PRAIRIE = "yellow"
+COULEUR_FEU = "red"
 
 ################################################
 # variables globales
@@ -44,13 +48,20 @@ def quadrillage():
         canvas.create_line((x, 0), (x, HAUTEUR), fill=COULEUR_QUADR)
         i += 1
 
+def remplire_carre(l,c,color):
+    """On remplit le carrée de coordonée (x, y)"""
+    y=l*COTE
+    x=c*COTE
+    for i in range(x, x+COTE):
+        canvas.create_line(i, y, i, y+COTE,fill=color)
+
 def xy_to_cl(x, y):
     """Retourne la colonne et la ligne correspondant au point du canevas de coordonnées (x,y)"""
     pass
 
 
 def change_carre(event):
-    """Change la couleur du carre à la position (event.x, event.y)"""
+    """Change la couleur du carre à la position (event.x, event.y) où on a cliqué"""
     pass
 
 ################################################
@@ -60,9 +71,15 @@ racine = tk.Tk()
 racine.title("Incendie")
 
 # création des widgets
-boutton_TERRAIN = tk.Button(racine, text = "TERRAIN", bg="grey", fg="blue", font=("Times", "20"))
+boutton_TERRAIN = tk.Button(racine, text = "TERRAIN", bg="grey", fg="blue", font=("Times", "20"), command =change_carre)
 canvas = tk.Canvas(racine, width = HAUTEUR, height = LARGEUR, bg = COULEUR_FOND)
-
+couleur_aleatoire =[COULEUR_EAU, COULEUR_FORET, COULEUR_PRAIRIE] #om met les couleurs des parcelles en liste
+lin = int(LARGEUR//COTE) 
+col = int(HAUTEUR//COTE)
+for ligne in range(lin):
+    for colonne in range(col):
+        color = couleur_aleatoire[random.randint(0,2)]
+        remplire_carre(ligne, colonne,color)
 # placement des widgets
 canvas.grid(row=0)
 boutton_TERRAIN.grid(row=1)
